4ae347ee1ff0c26ee982ca620ed2d48124d93b7f
BadWolf-alfa <marco+github@lunawolf.es>
2023-06-04 10:26:45 +0200

    wearui adapt

--- smtshell/app/src/main/java/com/samsung/SMT/lang/smtshell/AllTheButtons.java
+++ smtshell/app/src/main/java/com/samsung/SMT/lang/smtshell/AllTheButtons.java
@@ -1,20 +1,18 @@
 package com.samsung.SMT.lang.smtshell;
 
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.wear.widget.WearableLinearLayoutManager;
+import androidx.wear.widget.WearableRecyclerView;
+
+import com.samsung.SMT.lang.smtshell.watch.AdditionalWatchServices;
+import com.samsung.SMT.lang.smtshell.watch.CustomScrollingLayoutCallback;
+import com.samsung.SMT.lang.smtshell.watch.SMTCapability;
+import com.samsung.SMT.lang.smtshell.watch.WearableRecycleViewAdapter;
 
 import net.blufenix.smtshell.api.InternalAPI;
 import net.blufenix.smtshell.api.SMTShellAPI;
@@ -29,109 +27,65 @@ public class AllTheButtons extends AppCompatActivity {
             new SMTCapability(
                     "Kill SMT Shell API",
                     "Clears application data for com.samsung.SMT, disabling all payloads and allowing the exploit to run again.",
-                    "Kill", v -> {
-                InternalAPI.killAPI(this, success -> {
-                    ActivityUtils.launchNewTask(this, MainActivity.class);
-                });
-            }),
+                    "Kill", v -> InternalAPI.killAPI(this, success -> ActivityUtils.launchNewTask(this, MainActivity.class))),
             new SMTCapability(
                     "Local System Shell",
                     "Connect to a system shell directly in the app!",
-                    "Launch", v -> {
-                ActivityUtils.launch(AllTheButtons.this, ShellActivity.class);
-            }),
+                    "Launch", v -> ActivityUtils.launch(AllTheButtons.this, ShellActivity.class)),
             new SMTCapability(
                     "Reverse Shell",
                     "Start a reverse shell that can be connected to via:\n adb shell nc -l -p 9999",
-                    "Launch", v -> {
-                SMTShellAPI.loadLibrary(this, getApplicationInfo().nativeLibraryDir + "/" + "libsmtshell.so");
-            }),
+                    "Launch", v -> SMTShellAPI.loadLibrary(this, getApplicationInfo().nativeLibraryDir + "/" + "libsmtshell.so")),
             new SMTCapability(
                     "Band Selection",
                     null,
-                    "Launch", v -> {
-                SMTShellAPI.executeCommand(this, "am start com.samsung.android.app.telephonyui/.hiddennetworksetting.MainActivity");
-            }),
+                    "Launch", v -> SMTShellAPI.executeCommand(this, "am start com.samsung.android.app.telephonyui/.hiddennetworksetting.MainActivity")),
             new SMTCapability(
                     "Band Selection (Advanced)",
                     null,
-                    "Launch", v -> {
-                SMTShellAPI.executeCommand(this, "am broadcast -a com.samsung.android.action.SECRET_CODE -d android_secret_code://2263 -n com.sec.android.RilServiceModeApp/.SecKeyStringBroadcastReceiver");
-            }),
+                    "Launch", v -> SMTShellAPI.executeCommand(this, "am broadcast -a com.samsung.android.action.SECRET_CODE -d android_secret_code://2263 -n com.sec.android.RilServiceModeApp/.SecKeyStringBroadcastReceiver")),
             new SMTCapability(
                     "Change CSC",
                     null,
-                    "Launch", v -> {
-                SMTShellAPI.executeCommand(this, "am start -n com.samsung.android.cidmanager/.modules.preconfig.PreconfigActivity -a com.samsung.android.action.SECRET_CODE -d secret_code://27262826 --ei type 2");
-            }),
+                    "Launch", v -> SMTShellAPI.executeCommand(this, "am start -n com.samsung.android.cidmanager/.modules.preconfig.PreconfigActivity -a com.samsung.android.action.SECRET_CODE -d secret_code://27262826 --ei type 2")),
             new SMTCapability(
                     "Service Menu",
                     null,
-                    "Launch", v -> {
-                SMTShellAPI.executeCommand(this, "am broadcast -a com.samsung.android.action.SECRET_CODE -d android_secret_code://27663368378 -n com.sec.android.RilServiceModeApp/.SecKeyStringBroadcastReceiver");
-            }),
+                    "Launch", v -> SMTShellAPI.executeCommand(this, "am broadcast -a com.samsung.android.action.SECRET_CODE -d android_secret_code://27663368378 -n com.sec.android.RilServiceModeApp/.SecKeyStringBroadcastReceiver")),
             new SMTCapability(
                     "Info Menu",
                     null,
-                    "Launch", v -> {
-                SMTShellAPI.executeCommand(this, "am broadcast -a com.samsung.android.action.SECRET_CODE -d android_secret_code://0011 -n com.sec.android.RilServiceModeApp/.SecKeyStringBroadcastReceiver");
-            }),
+                    "Launch", v -> SMTShellAPI.executeCommand(this, "am broadcast -a com.samsung.android.action.SECRET_CODE -d android_secret_code://0011 -n com.sec.android.RilServiceModeApp/.SecKeyStringBroadcastReceiver")),
             new SMTCapability(
                     "DSU Loader",
                     null,
-                    "Launch", v -> {
-                SMTShellAPI.executeCommand(this, "am start -n com.android.settings/.development.DSULoader");
-            }),
+                    "Launch", v -> SMTShellAPI.executeCommand(this, "am start -n com.android.settings/.development.DSULoader")),
             new SMTCapability(
                     "Spawn DSU Notification",
                     "Spawns a notification in the system tray that allows a selected DSU to be discarded.",
-                    "Launch", v -> {
-                SMTShellAPI.executeCommand(this, "am start -n com.android.dynsystem/.VerificationActivity");
-            })
+                    "Launch", v -> SMTShellAPI.executeCommand(this, "am start -n com.android.dynsystem/.VerificationActivity"))
     ));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allthebuttons);
-        ListView listview = findViewById(R.id.list);
-        listview.setAdapter(new ArrayAdapter<SMTCapability>(this, R.layout.descriptive_button, list) {
-            @NonNull
-            @Override
-            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                if (convertView == null) {
-                    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    convertView = inflater.inflate(R.layout.descriptive_button, null);
-                }
 
-                SMTCapability item = getItem(position);
+        WearableRecyclerView wearableRecyclerView = findViewById(R.id.list);
+        wearableRecyclerView.setEdgeItemsCenteringEnabled(true);
 
-                TextView title = convertView.findViewById(R.id.title);
-                title.setText(item.title);
+        CustomScrollingLayoutCallback customScrollingLayoutCallback = new CustomScrollingLayoutCallback();
+        wearableRecyclerView.setLayoutManager(new WearableLinearLayoutManager(getApplicationContext(), customScrollingLayoutCallback));
 
-                TextView details = convertView.findViewById(R.id.details);
-                details.setText(item.desc);
+        wearableRecyclerView.setAdapter(new WearableRecycleViewAdapter(list));
 
-                Button btn = convertView.findViewById(R.id.btn);
-                btn.setText(item.btnText);
-                btn.setOnClickListener(item.onClick);
-
-                return convertView;
-            }
-        });
+        initAdditionalWatchServices(wearableRecyclerView);
     }
 
-    private static class SMTCapability {
-        String title;
-        String desc;
-        String btnText;
-        View.OnClickListener onClick;
-
-        SMTCapability(String title, String desc, String btnText, View.OnClickListener onClick) {
-            this.title = title;
-            this.desc = desc;
-            this.btnText = btnText;
-            this.onClick = onClick;
-        }
+    private void initAdditionalWatchServices(View view){
+        AdditionalWatchServices additionalWatchServices = new AdditionalWatchServices(getApplicationContext(), getSupportActionBar());
+        additionalWatchServices.customActionBar();
+        additionalWatchServices.bindHardwareRotary(view);
     }
+
 }
--- smtshell/app/src/main/java/com/samsung/SMT/lang/smtshell/MainActivity.java
+++ smtshell/app/src/main/java/com/samsung/SMT/lang/smtshell/MainActivity.java
@@ -6,22 +6,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.wear.remote.interactions.RemoteActivityHelper;
+import androidx.wear.widget.ConfirmationOverlay;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import com.samsung.SMT.lang.smtshell.shizuku.PackageInstallerUtils;
+import com.samsung.SMT.lang.smtshell.watch.AdditionalWatchServices;
 
 import net.blufenix.smtshell.api.SMTShellAPI;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import rikka.shizuku.Shizuku;
 
@@ -39,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
     private Button mExploitBtn;
     private ProgressBar mSpinner;
+
+    private LinearLayout noShizuku;
+    private String shizukuURL;
 
     private void onShizukuRequestPermissionsResult(int requestCode, int grantResult) {
         boolean granted = grantResult == PackageManager.PERMISSION_GRANTED;
@@ -88,11 +103,84 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.text);
         mExploitBtn = findViewById(R.id.btn_exploit);
         mSpinner = findViewById(R.id.indeterminateBar);
+        initAdditionalWatchServices();
+        initExtraButtons();
+    }
+
+    private void initAdditionalWatchServices(){
+        AdditionalWatchServices additionalWatchServices = new AdditionalWatchServices(getApplicationContext(), getSupportActionBar());
+        additionalWatchServices.customActionBar();
+        ScrollView scrollView = findViewById(R.id.scrollView);
+        additionalWatchServices.bindHardwareRotary(scrollView);
+    }
+
+    private void initExtraButtons(){
+        noShizuku = findViewById(R.id.noShizuku);
+        Button openReadMe = findViewById(R.id.open_readme);
+        Button openShizuku = findViewById(R.id.open_shizuku);
+
+        String ReadMeURL = "https://github.com/ITDev93/SMTShell-ForSamsungWatches#how-to-downgrade-smt";
+        openReadMe.setOnClickListener(view -> openURL(ReadMeURL));
+
+        shizukuURL = "https://github.com/RikkaApps/Shizuku/releases";
+        String shizukuPackage = "moe.shizuku.privileged.api";
+        openShizuku.setOnClickListener(view -> {
+            PackageManager pm = getPackageManager();
+            try {
+                pm.getPackageInfo(shizukuPackage, PackageManager.GET_ACTIVITIES);
+                startActivity(pm.getLaunchIntentForPackage(shizukuPackage));
+            } catch (PackageManager.NameNotFoundException e) {
+                openURL(shizukuURL);
+            }
+        });
+    }
+
+    private void openURL(String url){
+        Uri uriLink = Uri.parse(url);
+        Intent intentAndroid = new Intent(Intent.ACTION_VIEW)
+                .addCategory(Intent.CATEGORY_BROWSABLE)
+                .setData(uriLink);
+
+        Executor executor = Executors.newSingleThreadExecutor();
+        RemoteActivityHelper remoteActivityHelper = new RemoteActivityHelper(this,executor);
+        ListenableFuture<Void> result = remoteActivityHelper.startRemoteActivity(intentAndroid, null);
+        ConfirmationOverlay confirmationOverlay = new ConfirmationOverlay();
+        result.addListener(() -> {
+            try {
+                result.get();
+                CharSequence message = "Check your Phone";
+                runOnUiThread(() -> confirmationOverlay
+                        .setType(ConfirmationOverlay.OPEN_ON_PHONE_ANIMATION)
+                        .setMessage(message)
+                        .setDuration(3000)
+                        .setOnAnimationFinishedListener(() -> {
+                            if(url.equals(shizukuURL)){
+                                Toast.makeText(getApplicationContext(), "Shizuku not installed, install it on your watch!", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .showOn(this));
+            } catch (InterruptedException | ExecutionException e) {
+                CharSequence message = "Phone Can't be reached";
+                runOnUiThread(() -> confirmationOverlay
+                        .setType(ConfirmationOverlay.FAILURE_ANIMATION)
+                        .setMessage(message)
+                        .setDuration(3000)
+                        .showOn(this));
+            }
+        }, executor);
+
+    }
+
+    private void restartApp(){
+        Intent i = getIntent();
+        finish();
+        startActivity(i);
     }
 
     @Override
@@ -126,11 +214,12 @@ public class MainActivity extends AppCompatActivity {
     void setSpinner(boolean on) {
         mExploitBtn.setEnabled(!on);
         mSpinner.setProgress(0);
-        mSpinner.setVisibility(on ? View.VISIBLE : View.INVISIBLE);
+        mSpinner.setVisibility(on ? View.VISIBLE : View.GONE);
     }
 
     private void onReady(boolean shizuku) {
         if (shizuku && !isVulnerableSMT()) {
+            Toast.makeText(this, "Shizuku detected!", Toast.LENGTH_SHORT).show();
             mTextView.setText(R.string.downgrade_smt_prompt_shizuku);
             mExploitBtn.setText(R.string.downgrade_smt);
             mExploitBtn.setOnClickListener(v -> {
@@ -147,11 +236,16 @@ public class MainActivity extends AppCompatActivity {
                 });
             });
         } else if (!shizuku && !isVulnerableSMT()) {
-            mTextView.setText(R.string.downgrade_smt_prompt);
-            mExploitBtn.setText(R.string.proceed);
+            Toast.makeText(this, "Shizuku not detected!", Toast.LENGTH_SHORT).show();
+            noShizuku.setVisibility(View.VISIBLE);
+            mTextView.setText(R.string.downgrade_smt_prompt_restart);
+            mExploitBtn.setText(R.string.restart_app);
             mExploitBtn.setOnClickListener(v -> {
                 // user might grant permission here
-                onReady(Shizuku.pingBinder());
+                //onReady(Shizuku.pingBinder());
+                //Watch does not have task manager, leaving app destroys it,
+                //manual adb might work though but unlikely users would succeed, so better restart the app
+                restartApp();
             });
         } else {
             mTextView.setText(R.string.exploit_prompt);
